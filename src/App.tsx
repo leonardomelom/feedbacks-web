984794d60d101a15129f23a89c579be2a99bc7c1
Leonardo melo <44414965+lxnvrdx@users.noreply.github.com>
2022-05-02 23:22:53 -0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,22 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+interface ButtonProps {
+  text?: string;
+}
+ function Btn(props: ButtonProps) {
+
+    return (
+      <button className="bg-violet-600 py-2 px-4 rounded h-10 hover:bg-violet-800 transition-colors">{props.text}</button>
+    )
+  }
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='flex gap-2'>
+       <Btn text="Enviar" />
+       <Btn text="Send" />
     </div>
-  )
+    ) 
 }
 
 export default App
