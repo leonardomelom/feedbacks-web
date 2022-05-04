dbe4a95f5fbad8c627a737863423bb74fde3ad4f
Leonardo melo <44414965+lxnvrdx@users.noreply.github.com>
2022-05-03 23:41:48 -0300

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { useState } from 'react'
-
-interface ButtonProps {
-  text?: string;
-}
- function Btn(props: ButtonProps) {
-
-    return (
-      <button className="bg-violet-600 py-2 px-4 rounded h-10 hover:bg-violet-800 transition-colors">{props.text}</button>
-    )
-  }
-
+import { Widget } from './components/Widget'
 function App() {
   return (
-    <div className='flex gap-2'>
-       <Btn text="Enviar" />
-       <Btn text="Send" />
-    </div>
+  <Widget/>
     ) 
 }
 
