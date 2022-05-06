8394480d19c9bfc6a69657ef6cd0add42f7a15ae
Leonardo melo <44414965+lxnvrdx@users.noreply.github.com>
2022-05-05 21:53:59 -0300

    bolsonaro

--- src/components/WidgetForm/Index.tsx
+++ src/components/WidgetForm/Index.tsx
@@ -38,6 +38,7 @@ const [ feedbackType, setFeedbackType ] = useState<FeedbackType | null>(null)
 const[ feedbackSent, setFeedbackSent] = useState(false)
 
 function handleRestartFeedback(){
+  setFeedbackSent(false)
   setFeedbackType(null)
 }
   return(
@@ -45,7 +46,7 @@ function handleRestartFeedback(){
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
      {feedbackSent ? (
-       <FeedBackSucessStep />
+       <FeedBackSucessStep onFeedbackRestartRequested={handleRestartFeedback}/>
      ) : (
        <>
          {!feedbackType ? (
