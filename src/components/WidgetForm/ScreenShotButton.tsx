693f8dbde9d5df0715c2362a27c8a0faa79bca5c
Leonardo melo <44414965+lxnvrdx@users.noreply.github.com>
2022-05-08 10:17:52 -0300

    lula

---
+++ src/components/WidgetForm/ScreenShotButton.tsx
@@ -0,0 +1,42 @@
+import html2canvas from "html2canvas";
+import { Camera, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Loading } from "../Loading";
+
+
+interface ScreenShotButtonProps {
+  screenshot: string | null;
+  onScreenshotTook:(screenshot:string | null) => void;
+}
+export function ScreenShotButton({screenshot, onScreenshotTook}:ScreenShotButtonProps) {
+  const [isTakingScreenshot, setisTakingScreenshot] = useState(false);
+  async function handleTakeScreenshot(){
+    setisTakingScreenshot(true)
+    const canvas = await html2canvas(document.querySelector('html')!);
+    const base64image = canvas.toDataURL('image/png');
+    
+    onScreenshotTook(base64image)
+    setisTakingScreenshot(false)
+  }
+  if(screenshot){
+    return(
+     <button type="button" className="p-1 w-10 h-10 border-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-color"
+     style={{
+        backgroundImage: `url(${screenshot})`,
+        backgroundPosition: 'right bottom',
+        backgroundSize: 180,
+     }}
+     onClick={() => onScreenshotTook(null)}>
+       <Trash weight="fill"/>
+     </button>
+    )
+  }
+return(
+  <button
+  type='button'
+  className='p-2  bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500'
+  onClick={handleTakeScreenshot}
+  >
+    {isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6  " />}
+  </button>
+)}
